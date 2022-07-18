30cca8474449e204d18f76a5b33c20f69e63cd56
Toukir <toukir08@gmail.com>
2022-07-18 21:40:19 +0930

    Added prismic

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,9 +1,13 @@
 import type { GatsbyConfig } from "gatsby"
 
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `React-Typescript-Gatsby-Prismic`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://www.github-demo.com`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -18,7 +22,19 @@ const config: GatsbyConfig = {
           require("./tailwind.config.js"), // Optional: Load custom Tailwind CSS configuration
         ],
       },
-    }
+    },
+    'gatsby-plugin-image',
+    {
+      resolve: 'gatsby-source-prismic',
+      options: {
+        repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
+        accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+        linkResolver: require('./src/utils/linkResolver').linkResolver,
+        schemas: {
+          popup: require('./custom_types/popup.json'),
+        }
+      },
+    },
   ],
 }
 
