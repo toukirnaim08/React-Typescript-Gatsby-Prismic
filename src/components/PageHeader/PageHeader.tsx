c50c58d63c8830ec01fef02168900abea64e438d
Toukir <toukir08@gmail.com>
2022-07-19 09:49:29 +0930

    Added shorthand declaration

--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -1,9 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import logo1 from '../../images/logo1.png';
 import './PageHeader.scss'
-
-const MAX_POP_UP_DELAY = 2 // days
-const COOKIE_KEY = 'POPUP_CLOSE_TIME'
 
 export default function PageHeader() {
   return (
